4020140616e79648b23534b0e94e1adf10ac39c9
Alexey Shevchenko <os@consulting.mk>
2021-11-30 22:42:32 +0200

    fixed active filters state condition

--- src/app/pages/search/components/select-search-button/select-search-button.component.ts
+++ src/app/pages/search/components/select-search-button/select-search-button.component.ts
@@ -41,12 +41,11 @@ export class SelectSearchButtonComponent implements OnChanges {
 			return;
 		}
 		let groups = {};
-		let stringifiedActiveFilters = this.filters_active.map(it => JSON.stringify(it));
 		this.filters_all.forEach(f => {
 			let groupname = this.i18n.translateVariable(f.group || '_');
 			groups[groupname] = groups[groupname] || {name: groupname, filters: []};
 			groups[groupname].filters.push({
-				active: stringifiedActiveFilters.indexOf(JSON.stringify(f)) >= 0,
+				active: this.filters_active.some(filter => filter.id === f.id),
 				isSearch: isSearchDef(f),
 				isFilter: isFilterDef(f),
 				filter: f,
